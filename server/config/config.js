e8179d3b381c88b06f9ac47cde6861263fbf918e
alsrlqor <skyup9393@gmail.com>
2022-02-03 04:07:13 +0900

    Database Changed

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DEV_DATABASE_NAME,
-    //port: process.env.DATABASE_PORT,
+    port: process.env.DATABASE_PORT,
     host: process.env.DATABASE_HOST,
     dialect: 'mysql'
   },
