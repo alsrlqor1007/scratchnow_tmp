ab141725bb30d81baeb6af1d2eac4947b41b8ecc
ciocio97 <tmddusleo@icloud.com>
2022-02-03 16:04:09 +0900

    template : create signIn, signUp page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Navigation from '../src/components/navigation';
+import { SignIn, SignUp } from './pages/signInSignUp';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Navigation />}/>
+        <Route path="/signIn" element={<SignIn />}/>
+        <Route path="/signUp" element={<SignUp />}/>
       </Routes>
     </>
   );
