5e774cce932dc9118923a96acfa4f33d174ba8f5
ciocio97 <tmddusleo@icloud.com>
2022-02-03 18:58:14 +0900

    feature : add signIn, signUp, logout function

--- client/src/pages/signInSignUp.js
+++ client/src/pages/signInSignUp.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const Container = styled.div`
@@ -54,6 +55,9 @@ const ErrorMessage = styled.div`
   margin: 5px 0px 25px 0px;
   font-size: 0.75em;
   color: #E57A75;
+  ${props => props.login &&`
+    margin 5px 0px;
+  `}
 `
 const Button = styled.button`
   width: 300px;
@@ -112,19 +116,63 @@ export function SignIn () {
   const navigate = useNavigate();
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
-  const handleInput = (event, func) => {
-    func(event.target.value);
+  const [emailState, setEmailState] = useState(true);
+  const [passwordState, setPasswordState] = useState(true);
+  const checkEmail = (event) => {
+    const email = event.target.value;
+    const emailCheckRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    if(email.match(emailCheckRegExp) !== null){
+      setEmailState(true);
+    } else {
+      setEmailState(false);
+    }
+    setEmail(email);
+  }
+  const checkPassword = (event) => {
+    const password = event.target.value;
+    const passwordCheckRegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    if(password.match(passwordCheckRegExp) !== null){
+      setPasswordState(true);
+    } else {
+      setPasswordState(false);
+    }
+    setPassword(password);
+  }
+  const signIn = () => {
+    if(emailState && passwordState){
+      axios({
+        url: 'http://ec2-52-78-171-4.ap-northeast-2.compute.amazonaws.com/api/sign/login',
+        method: 'post',
+        data: {
+          email,
+          password,
+        },
+        withCredentials: true,
+      })
+      .then((res) => {
+        if(res.status === 200){
+          console.log(res);
+          localStorage.setItem('isLogin', true);
+          navigate('/'); // 로그인 성공 후 메인 페이지 이동
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    } else {
+      alert('모든 형식을 알맞게 채워주세요.') // 일단
+    }
   }
   return (
     <>
       <Container>
         <Box>
           <InnerBox>
-            <InputBox placeholder="이메일" onChange={(e) => handleInput(e, setEmail)} />
-            <ErrorMessage>이메일 형식 에러메세지</ErrorMessage>
-            <InputBox placeholder="비밀 번호" onChange={(e) => handleInput(e, setPassword)}/>
-            <ErrorMessage>비밀 번호 형식 에러메세지</ErrorMessage>
-            <Button>로그인</Button>
+            <InputBox placeholder="이메일" onChange={checkEmail} />
+            <ErrorMessage>{emailState ? '' : '이메일 주소가 유효하지 않습니다.'}</ErrorMessage>
+            <InputBox placeholder="비밀 번호" onChange={checkPassword}/>
+            <ErrorMessage>{passwordState ? '' : '유효한 비밀 번호가 아닙니다.'}</ErrorMessage>
+            <Button onClick={signIn}>로그인</Button>
             <DividedBox>
               <SplitLine/>
               또는
@@ -145,12 +193,75 @@ export function SignIn () {
 export function SignUp () {
   const navigate = useNavigate();
   const [email, setEmail] = useState(null);
-  const [nickName, setNickName] = useState(null);
+  const [nickname, setNickname] = useState(null);
   const [password, setPassword] = useState(null);
-  const [confirmedPassword, setConfirmedPassword] = useState(null);
-  const handleInput = (event, func) => {
-    func(event.target.value);
+  const [emailState, setEmailState] = useState(true);
+  const [nickNameState, setNickNameState] = useState(true);
+  const [passwordState, setPasswordState] = useState(true);
+  const [samePasswordState, setSamePasswordState] = useState(true);
+  const checkEmail = (event) => {
+    const email = event.target.value;
+    const emailCheckRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    if(email.match(emailCheckRegExp) !== null){
+      setEmailState(true);
+    } else {
+      setEmailState(false);
+    }
+    setEmail(email);
+  };
+  const checkNickName = (event) => {
+    const nickName = event.target.value;
+    if(nickName.length <= 10){
+      setNickNameState(true);
+    } else {
+      setNickNameState(false);
+    }
+    setNickname(nickName);
+  };
+  const checkPassword = (event) => {
+    const password = event.target.value;
+    const passwordCheckRegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    if(password.match(passwordCheckRegExp) !== null){
+      setPasswordState(true);
+    } else {
+      setPasswordState(false);
+    }
+    setPassword(password);
+  };
+  const checkPasswordIsSame = (event) => {
+    const retypedPassword = event.target.value;
+    if(retypedPassword === password){
+      setSamePasswordState(true);
+    } else {
+      setSamePasswordState(false);
+    }
+  };
+  const signUp = () => {
+    if(emailState && nickNameState && passwordState && samePasswordState){
+      axios({
+        url: 'http://ec2-52-78-171-4.ap-northeast-2.compute.amazonaws.com/api/sign/register',
+        method: 'post',
+        data: {
+          email,
+          nickname,
+          password,
+        },
+        // withCredentials: true,
+      })
+      .then((res) => {
+        if(res.status === 201){
+          console.log(res);
+          navigate('/signIn'); // 회원 가입 후 로그인 페이지 이동
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    } else {
+      alert('모든 형식을 알맞게 채워주세요.') // 일단
+    }
   }
+  
   return (
     <>
       <Container>
@@ -162,15 +273,15 @@ export function SignUp () {
               또는
               <SplitLine/>
             </DividedBox>
-            <InputBox placeholder="이메일" onChange={(e) => handleInput(e, setEmail)}/>
-            <ErrorMessage>이메일 형식 에러메세지</ErrorMessage>
-            <InputBox placeholder="닉네임" onChange={(e) => handleInput(e, setNickName)}/>
-            <ErrorMessage>닉네임 형식 에러메세지</ErrorMessage>
-            <InputBox placeholder="비밀 번호" onChange={(e) => handleInput(e, setPassword)}/>
-            <ErrorMessage>비밀 번호 형식 에러메세지</ErrorMessage>
-            <InputBox placeholder="비밀 번호 확인" onChange={(e) => handleInput(e, setConfirmedPassword)}/>
-            <ErrorMessage>비밀 번호 확인 에러메세지</ErrorMessage>
-            <Button signUp>로그인</Button>
+            <InputBox placeholder="이메일" onChange={checkEmail}/>
+            <ErrorMessage>{emailState ? '' : '이메일 주소가 유효하지 않습니다.'}</ErrorMessage>
+            <InputBox placeholder="닉네임" onChange={checkNickName}/>
+            <ErrorMessage>{nickNameState ? '' : '닉네임은 10자 이내로 입력해주세요.'}</ErrorMessage>
+            <InputBox placeholder="비밀 번호" onChange={checkPassword}/>
+            <ErrorMessage>{passwordState ? '' : '숫자, 대문자, 특수 문자를 포함하여 8자 이상이어야 합니다.'}</ErrorMessage>
+            <InputBox placeholder="비밀 번호 확인" onChange={checkPasswordIsSame}/>
+            <ErrorMessage>{samePasswordState ? '' : '비밀 번호가 일치하지 않습니다.'}</ErrorMessage>
+            <Button signUp onClick={signUp}>회원 가입</Button>
             <BottomLink>회원이신가요 ? 
                 <Bold onClick={() => navigate('/signIn')}>로그인 하기</Bold>
             </BottomLink>
