afae1a358a767a607bc09d78cd48bb0ee69ae14f
alsrlqor <skyup9393@gmail.com>
2022-02-05 05:20:34 +0900

    Server Update

--- server/routes/PostRouter.js
+++ server/routes/PostRouter.js
@@ -9,7 +9,7 @@ const upload = multer({
     storage: multer.diskStorage({
         // 저장 공간
         destination: (req, file, done) => {
-            done(null, '/server/uploads/paintings/');
+            done(null, 'paintings/');
         },
         // 저장 이름
         filename: (req, file, done) => {
