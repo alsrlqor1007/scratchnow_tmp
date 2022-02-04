e21a32f3d14153f8d5f4eb0829c7cc4c4bebe729
alsrlqor <skyup9393@gmail.com>
2022-02-04 20:44:38 +0900

    multer inuse

--- server/routes/UserinfoRouter.js
+++ server/routes/UserinfoRouter.js
@@ -10,7 +10,7 @@ const upload = multer({
     storage: multer.diskStorage({
         // 저장 공간
         destination: (req, file, done) => {
-            done(null, 'uploads/');
+            done(null, 'uploads/profile_img');
         },
         // 저장 이름
         filename: (req, file, done) => {
